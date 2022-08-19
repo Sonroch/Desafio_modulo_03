bbff0553ee2e3cc0d83e8a6bcf085c103bb0ba8f
Sonroch <sonroch@gmail.com>
2022-08-18 22:45:07 -0300

    configuracao heroku correta

--- src/conexao.js
+++ src/conexao.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
     user: "lodjcxlfcfrdkc",
     host: "ec2-44-205-112-253.compute-1.amazonaws.com",
-    database: "dindin",
+    database: "d1dks33n3r4322",
     password: "3ab217a5289c7285653612fa15ab516252ef7026677d2986f9a9f6d383a503fd",
     port: 5432,
     ssl: {
