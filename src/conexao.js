9f6517cfb6e2d3f660b18cb41c0812327fb97517
Sonroch <sonroch@gmail.com>
2022-08-23 15:50:20 -0300

    teste com env

--- src/conexao.js
+++ src/conexao.js
@@ -1,6 +1,6 @@
-const knex = require('knex')({
-    client: "pg",
-    connection:{
+const { Pool } = require("pg");
+
+const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
@@ -10,9 +10,11 @@ const knex = require('knex')({
         rejectUnauthorized: false,
     },
 
-}});
+});
 
+const query = (text, param) => {
+    return pool.query(text, param);
+};
 
-
-module.exports = knex
+module.exports = { query }
 
