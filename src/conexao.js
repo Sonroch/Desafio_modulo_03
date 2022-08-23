9981e545355c48dcbed15189c7b0d939eefea89d
Sonroch <sonroch@gmail.com>
2022-08-23 11:48:51 -0300

    ajustando conection

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "lodjcxlfcfrdkc",
-    host: "ec2-44-205-112-253.compute-1.amazonaws.com",
-    database: "d1dks33n3r4322",
-    password: "3ab217a5289c7285653612fa15ab516252ef7026677d2986f9a9f6d383a503fd",
+    user: "qgocbdmvudzllx",
+    host: "ec2-18-208-55-135.compute-1.amazonaws.com",
+    database: "de3u7v04kk281p",
+    password: "71ce8d63ff569a7eab3e3b8d3a3fc1836fd840fc9549e030e8fdac3431353612",
     port: 5432,
     ssl: {
         rejectUnauthorized: false,
